bce50fb709542da71433e69705b3b02a96b7f60a
seanriordan08 <sean.riordan08@gmail.com>
2016-01-28 14:25:50 -0700

    Add modal view, action link hover behavior

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
   $('.button-collapse').sideNav();
   $('.parallax').parallax();
+  $(".dropdown-button").dropdown({hover: true});
+  $('.modal-trigger').leanModal({
+    opacity: .5
+  });
 });
