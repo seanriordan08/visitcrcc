9ada2655f04b82db2ff86fc4cb422f6ce0dc87a9
seanriordan08 <sean.riordan08@gmail.com>
2016-01-29 09:55:03 -0700

    Fix turbolinks

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,8 +1,18 @@
-$(document).on("page:change",function(){
+$(document).ready(function(){
   $('.button-collapse').sideNav();
   $('.parallax').parallax();
   $(".dropdown-button").dropdown({hover: true});
+
   $('.modal-trigger').leanModal({
     opacity: .5
   });
+
+  $('.collapsible-header').one("click", function(){
+    $('.modal-content').animate({
+        scrollTop: $('.modal-content').height()
+      },
+      300,
+      "easeOutQuint"
+    );
+  })
 });
