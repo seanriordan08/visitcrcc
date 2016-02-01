c84aa69b9d8c9fd65cb81ae87cf3ad988d1926a7
seanriordan08 <sean.riordan08@gmail.com>
2016-02-01 14:21:50 -0700

    Remove csrf checking for index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,5 @@
 class WelcomeController < ApplicationController
 
-  before_action :allow_external_url, only: :index
-  skip_before_filter :verify_authenticity_token, :only => [:index]
-
   def index
     if Rails.env.development?
       @biblia_key = Rails.application.secrets.biblia_api_dev_key
@@ -12,15 +9,6 @@ class WelcomeController < ApplicationController
   end
 
   def staff
-  end
-
-  private
-
-  def allow_external_url
-    headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
-    headers['Access-Control-Request-Method'] = '*'
-    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   end
 
 end
