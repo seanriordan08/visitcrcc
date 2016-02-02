ceb58519d8ac9f7ca2976183d64586e31a23720c
seanriordan08 <sean.riordan08@gmail.com>
2016-02-01 17:37:28 -0700

    Add cors gem params

--- config/application.rb
+++ config/application.rb
@@ -30,20 +30,10 @@ module Visitcrcc
     }
 
     #Required to make Devise play nice with CORS (ie. as required by Biblia)
-    config.middleware.insert_before Warden::Manager, "Rack::Cors", :debug => true, :logger => (-> { Rails.logger }) do
+    config.middleware.insert_before 0, "Rack::Cors" do
       allow do
         origins '*'
-
-        resource '/cors',
-         :headers => :any,
-         :methods => [:post],
-         :credentials => true,
-         :max_age => 0
-
-        resource '*',
-         :headers => :any,
-         :methods => [:get, :post, :delete, :put, :patch, :options, :head],
-         :max_age => 0
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
       end
     end
 
