c52bd51e3bc9e174b107473b2ff8d3cf96306c31
seanriordan08 <sean.riordan08@gmail.com>
2016-02-21 20:16:58 -0700

    remove default account first last name

--- db/migrate/20160201183819_add_first_lastname.rb
+++ db/migrate/20160201183819_add_first_lastname.rb
@@ -1,6 +1,6 @@
 class AddFirstLastname < ActiveRecord::Migration
   def change
-    add_column :users, :first_name, :string, default: "John or Jane"
-    add_column :users, :last_name, :string, default: "Doe"
+    add_column :users, :first_name, :string, default: "", null: false
+    add_column :users, :last_name, :string, default: "", null: false
   end
 end
