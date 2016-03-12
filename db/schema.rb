cab3bc086b37e77d88b1565a1de1f78c4793e760
seanriordan08 <sean.riordan08@gmail.com>
2016-03-12 12:39:01 -0700

    Add avatars to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160311025321) do
+ActiveRecord::Schema.define(version: 20160312193342) do
 
   create_table "events", force: :cascade do |t|
     t.string   "title",       limit: 255,   default: "Event Title", null: false
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20160311025321) do
     t.string   "last_name",              limit: 255, default: "",      null: false
     t.string   "role_description",       limit: 255, default: "guest"
     t.boolean  "staff",                              default: false
+    t.string   "avatar",                 limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
