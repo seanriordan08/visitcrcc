00fd4bf030b34f404c9bdbc996a7fee4b4762565
seanriordan08 <sean.riordan08@gmail.com>
2016-03-12 15:45:35 -0700

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable
 
+  validates :first_name, :last_name, :gender, presence: true
+
   has_one :avatar
   has_and_belongs_to_many :events
 
