95e98db2420e48e4bd63576e783941ad28475c6c
seanriordan08 <sean.riordan08@gmail.com>
2016-03-14 08:18:06 -0600

    Add remaining avatars

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -24,4 +24,17 @@ module ApplicationHelper
     SecureRandom.base64(24)
   end
 
+  def all_avatar_faces
+    if Rails.env.development?
+      [['Square','Face-1'],['Patch','Face-2'],['Round','Face-3'],['Shaggy','Face-4'],['McFly','Face-5'], ['GlassNoPart','Face-6']]
+    else
+      [
+        ['Square','Face-1'],['Patch','Face-2'],['Round','Face-3'],['Shaggy','Face-4'],['McFly','Face-5'],
+        ['GlassNoPart','Face-6'],['Shaft','Face-7'],['Mono','Face-8'],['MiddlePart','Face-9'],['SidePart','Face-10'],
+        ['Ring','Face-11'],['FiveOclock','Face-12'],['Chin','Face-13'],['Burns','Face-14'],['CleanShort','Face-15'],
+        ['CleanLong','Face-16'],['Stash','Face-17'],['Dome','Face-18'],['Buzz','Face-19'],['BuzzPeak','Face-20']
+      ]
+    end
+  end
+
 end
