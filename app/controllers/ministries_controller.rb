b3bee277725fbac484a63d59b003b1f66ea02186
seanriordan08 <sean.riordan08@gmail.com>
2016-04-03 17:49:28 -0600

    Add admin editable content to ministry pages

--- app/controllers/ministries_controller.rb
+++ app/controllers/ministries_controller.rb
@@ -3,6 +3,7 @@ class MinistriesController < ApplicationController
 
   def life_groups
     @lead_pastor = User.where(role_description: "Lead Pastor").first
+    @html_content = WelcomeContent.where(page_name: 'life_groups').first.html_content
   end
 
   def mens
@@ -12,46 +13,57 @@ class MinistriesController < ApplicationController
 
   def mens_serve
     @lead_pastor = User.where(role_description: "Lead Pastor").first
+    @html_content = WelcomeContent.where(page_name: 'life_groups').first.html_content
   end
 
   def womens
     @womens_director = User.where(role_description: "Women's Ministry Director").first
+    @html_content = WelcomeContent.where(page_name: 'womens').first.html_content
   end
 
   def womens_serve
     @womens_director = User.where(role_description: "Women's Ministry Director").first
+    @html_content = WelcomeContent.where(page_name: 'womens_serve').first.html_content
   end
 
   def youth
     @lead_pastor = User.where(role_description: "Lead Pastor").first
+    @html_content = WelcomeContent.where(page_name: 'youth').first.html_content
   end
 
   def youth_serve
     @lead_pastor = User.where(role_description: "Lead Pastor").first
+    @html_content = WelcomeContent.where(page_name: 'youth_serve').first.html_content
   end
 
   def childrens
     @childrens_pastor = User.where(role_description: "Children's Ministry Pastor").first
+    @html_content = WelcomeContent.where(page_name: 'childrens').first.html_content
   end
 
   def childrens_serve
     @childrens_pastor = User.where(role_description: "Children's Ministry Pastor").first
+    @html_content = WelcomeContent.where(page_name: 'childrens_serve').first.html_content
   end
 
   def worship_arts
     @worship_pastor = User.where(role_description: "Worship Pastor").first
+    @html_content = WelcomeContent.where(page_name: 'worship_arts').first.html_content
   end
 
   def worship_arts_serve
     @lead_pastor = User.where(role_description: "Lead Pastor").first
+    @html_content = WelcomeContent.where(page_name: 'worship_arts_serve').first.html_content
   end
 
   def missions
     # @youth_pastor = User.where(role_description: "Youth Pastor").first
+    @html_content = WelcomeContent.where(page_name: 'missions').first.html_content
   end
 
   def missions_serve
     # @youth_pastor = User.where(role_description: "Youth Pastor").first
+    @html_content = WelcomeContent.where(page_name: 'missions_serve').first.html_content
   end
 
   def save_html_content
@@ -59,7 +71,6 @@ class MinistriesController < ApplicationController
     welcome_content.update(html_content: params[:html_content])
 
     respond_to do |format|
-      # flash[:error] = "Unable to save changes :(" unless welcome_content
       format.js { render "ministry_page" }
     end
   end
