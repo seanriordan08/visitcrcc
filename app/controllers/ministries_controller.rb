09c96118ebdee2d8a6b8fb30f6c6d54a313932b9
seanriordan08 <sean.riordan08@gmail.com>
2016-04-05 22:55:14 -0600

    Add timestamps to editable content

--- app/controllers/ministries_controller.rb
+++ app/controllers/ministries_controller.rb
@@ -3,67 +3,67 @@ class MinistriesController < ApplicationController
 
   def life_groups
     @lead_pastor = User.where(role_description: "Lead Pastor").first
-    @html_content = WelcomeContent.where(page_name: 'life_groups').first.html_content
+    @content = WelcomeContent.where(page_name: 'life_groups').first
   end
 
   def mens
     @lead_pastor = User.where(role_description: "Lead Pastor").first
-    @html_content = WelcomeContent.where(page_name: 'mens').first.html_content
+    @content = WelcomeContent.where(page_name: 'mens').first
   end
 
   def mens_serve
     @lead_pastor = User.where(role_description: "Lead Pastor").first
-    @html_content = WelcomeContent.where(page_name: 'mens_serve').first.html_content
+    @content = WelcomeContent.where(page_name: 'mens_serve').first
   end
 
   def womens
     @womens_director = User.where(role_description: "Women's Ministry Director").first
-    @html_content = WelcomeContent.where(page_name: 'womens').first.html_content
+    @content = WelcomeContent.where(page_name: 'womens').first
   end
 
   def womens_serve
     @womens_director = User.where(role_description: "Women's Ministry Director").first
-    @html_content = WelcomeContent.where(page_name: 'womens_serve').first.html_content
+    @content = WelcomeContent.where(page_name: 'womens_serve').first
   end
 
   def youth
     @lead_pastor = User.where(role_description: "Lead Pastor").first
-    @html_content = WelcomeContent.where(page_name: 'youth').first.html_content
+    @content = WelcomeContent.where(page_name: 'youth').first
   end
 
   def youth_serve
     @lead_pastor = User.where(role_description: "Lead Pastor").first
-    @html_content = WelcomeContent.where(page_name: 'youth_serve').first.html_content
+    @content = WelcomeContent.where(page_name: 'youth_serve').first
   end
 
   def childrens
     @childrens_pastor = User.where(role_description: "Children's Ministry Pastor").first
-    @html_content = WelcomeContent.where(page_name: 'childrens').first.html_content
+    @content = WelcomeContent.where(page_name: 'childrens').first
   end
 
   def childrens_serve
     @childrens_pastor = User.where(role_description: "Children's Ministry Pastor").first
-    @html_content = WelcomeContent.where(page_name: 'childrens_serve').first.html_content
+    @content = WelcomeContent.where(page_name: 'childrens_serve').first
   end
 
   def worship_arts
     @worship_pastor = User.where(role_description: "Worship Pastor").first
-    @html_content = WelcomeContent.where(page_name: 'worship_arts').first.html_content
+    @content = WelcomeContent.where(page_name: 'worship_arts').first
   end
 
   def worship_arts_serve
     @lead_pastor = User.where(role_description: "Lead Pastor").first
-    @html_content = WelcomeContent.where(page_name: 'worship_arts_serve').first.html_content
+    @content = WelcomeContent.where(page_name: 'worship_arts_serve').first
   end
 
   def missions
     # @youth_pastor = User.where(role_description: "Youth Pastor").first
-    @html_content = WelcomeContent.where(page_name: 'missions').first.html_content
+    @content = WelcomeContent.where(page_name: 'missions').first
   end
 
   def missions_serve
     # @youth_pastor = User.where(role_description: "Youth Pastor").first
-    @html_content = WelcomeContent.where(page_name: 'missions_serve').first.html_content
+    @content = WelcomeContent.where(page_name: 'missions_serve').first
   end
 
   def save_html_content
