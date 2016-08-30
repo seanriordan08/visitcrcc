6f781ffb6fed2dfb931b716123e54c6d53ec26d9
seanriordan08 <sean.riordan08@gmail.com>
2016-08-30 15:18:34 -0700

    Add inquiries

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160817022456) do
+ActiveRecord::Schema.define(version: 20160830220049) do
 
   create_table "avatars", force: :cascade do |t|
     t.integer  "user_id",          limit: 4
@@ -43,6 +43,18 @@ ActiveRecord::Schema.define(version: 20160817022456) do
     t.string   "last_modified_by",   limit: 255
     t.datetime "last_modified_date"
     t.boolean  "active",                           default: false
+  end
+
+  create_table "inquiries", force: :cascade do |t|
+    t.string   "email",          limit: 255
+    t.boolean  "registered"
+    t.string   "media_origin",   limit: 255, default: "web_form"
+    t.string   "tags",           limit: 255
+    t.integer  "priority",       limit: 4,   default: 0
+    t.boolean  "responded",                  default: false
+    t.datetime "created_at",                                      null: false
+    t.datetime "updated_at",                                      null: false
+    t.integer  "helpful_rating", limit: 4
   end
 
   create_table "life_group_demographics", force: :cascade do |t|
