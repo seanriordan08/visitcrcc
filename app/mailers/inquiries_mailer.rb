8f63bfcb87d03bbc26db5bb25f9775d5940c27f5
seanriordan08 <sean.riordan08@gmail.com>
2016-09-03 17:17:00 -0700

    Update address

--- app/mailers/inquiries_mailer.rb
+++ app/mailers/inquiries_mailer.rb
@@ -1,10 +1,9 @@
 class InquiriesMailer < ApplicationMailer
-  default from: 'info@visitcrcc.org'
 
   def inquiry_email(user_email, email_body)
     @email = user_email
     @body = email_body
-    mail(to: 'sean.riordan08@gmail.com', subject: 'Inquiry (from website)')
+    mail(to: 'info@visitcrcc.org', subject: 'Inquiry (from website)')
   end
 
 end
