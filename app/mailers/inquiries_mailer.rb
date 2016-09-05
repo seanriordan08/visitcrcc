8e6382d785d0e4ca330c3ae3fc6943355533954c
seanriordan08 <sean.riordan08@gmail.com>
2016-09-05 12:31:05 -0600

    Wire up info address for contact form

--- app/mailers/inquiries_mailer.rb
+++ app/mailers/inquiries_mailer.rb
@@ -4,7 +4,7 @@ class InquiriesMailer < ApplicationMailer
   def inquiry_email(user_email, email_body)
     @email = user_email
     @body = email_body
-    mail(to: 'sean.riordan08@gmail.com', from: @email, subject: 'Inquiry (from website)', reply_to: @email)
+    mail(to: 'info@visitcrcc.org', from: @email, subject: 'Inquiry (from website)', reply_to: @email)
   end
 
 end
